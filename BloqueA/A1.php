2aca46a540283b46845cb9279517d697b48b783f
Rubén <rubenramirezinformatica@gmail.com>
2024-10-31 10:13:45 +0100

    Create A1.php
    Ejercicio1

---
+++ BloqueA/A1.php
@@ -0,0 +1,16 @@
+<?php 
+$name = 'Rubén';
+$price = 2;
+?>
+
+<!DOCTYPE html>
+<head>
+    <title>Variables</title>
+</head>
+<body>
+    <h1>The Candy Store</h1>
+    <h2> Welcome  <?php echo $name; ?> </h2>
+    <p> The cost of your candy is 
+        <?php echo $price; ?>€ per  pack. </p>
+</body>
+</html>
